509130367bd0c74ff2a975a1925dc2907479b94c
jjong_0425 <bfpark99@naver.com>
2025-01-24 12:21:08 +0900

    chore : TypeORM 사용 기본 설정(config, module 등)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,15 @@
 import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
 import { BlogsModule } from './blog/blogs.module';
-import { ConfigModule } from '@nestjs/config';
-import { databaseConfig } from './configs/database.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './configs/typeorm.config';
 
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(typeOrmConfig),
     BoardsModule,
     BlogsModule
   ],
-  controllers : [],
-  providers: [
-    {
-      provide : 'DATABASE_CONFIG',
-      useValue : databaseConfig
-    }
-  ],
-  exports: ['DATABASE_CONFIG']
 })
 export class AppModule {}
--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
-import { BoardsRepository } from './boards.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Board } from './boards.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Board]) // Board 엔터티를 TypeORM 모듈에 등록
+  ],
   controllers: [BoardsController],
-  providers: [BoardsService,BoardsRepository],
+  providers: [BoardsService],
 })
 export class BoardsModule {}
