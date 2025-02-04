05e0f368c62f705f4bdff02a23f1205dd3c9e1a7
jjong_0425 <bfpark99@naver.com>
2025-02-04 14:50:48 +0900

    feat : [2] JWT를 쿠키에 저장

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Post, Res, UsePipes, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserResponseDto } from './dto/user-response.dto';
 import { LoginUserDto } from './dto/login-user.dto';
+import { Response } from 'express';
 
 @Controller('api/auth')
 @UsePipes(ValidationPipe)
@@ -18,8 +19,16 @@ export class AuthController {
 
     // 로그인 기능
     @Post('/signin')
-    async signIn(@Body() LoginUserDto: LoginUserDto){
-        const message = this.authService.signIn(LoginUserDto);
-        return message;
+    async signIn(@Body() loginUserDto: LoginUserDto, @Res() res:Response): Promise<void>{
+        const accessToken = await this.authService.signIn(loginUserDto);
+        
+        //[2] JWT를 쿠키에 저장
+        res.cookie(`Authorization`, accessToken,{
+            httpOnly: true,
+            secure: false,
+            maxAge: 360000,
+            sameSite: `none`
+        })
+        res.send({message: "Login Success"});
     }
 }
