31d811a2339087fd48a2e58a026ebe14d88a1508
jjong_0425 <bfpark99@naver.com>
2025-02-05 12:01:33 +0900

    feat : 인증 가드를 활용한 회원 역할별 접근 권한 제어(인가)

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Post, Query, Patch, Put, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Query, Patch, Put, UsePipes, ValidationPipe, UseGuards } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board } from './boards.entity';
 import { BoardResponseDto } from './dto/board-response.dto';
@@ -6,14 +6,20 @@ import { CreateBoardDto } from './dto/create-board.dto';
 import { UpdateBoardDto } from './dto/update-board.dto';
 import { BoardStatus } from './boards-status.enum';
 import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from 'src/auth/custom-role.guard';
+import { UserRole } from 'src/auth/users-role.enum';
+import { Roles } from 'src/auth/roles.decorator';
 
 @Controller('api/boards')
+@UseGuards(AuthGuard(), RolesGuard)
 export class BoardsController {
     // 생성자 주입
     constructor(private boardsService: BoardsService){}
 
     // 게시글 조회 기능 
     @Get('/')
+    @Roles(UserRole.USER)  // 로그인유저가 USER만 접근 가능
     async getAllBoards(): Promise<BoardResponseDto[]> {
         const boards: Board[] = await this.boardsService.getAllBoards();
         const boardsResponseDto = boards.map(board => new BoardResponseDto(board));
--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -3,9 +3,11 @@ import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Board } from './boards.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   imports: [
+    AuthModule,
     TypeOrmModule.forFeature([Board]) // Board 엔터티를 TypeORM 모듈에 등록
   ],
   controllers: [BoardsController],
