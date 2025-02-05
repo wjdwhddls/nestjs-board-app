0312fbba87112df2549468ca030b343ca0e9f225
jjong_0425 <bfpark99@naver.com>
2025-02-05 11:25:22 +0900

    feat : 커스텀 데코레이터 생성

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,6 +5,8 @@ import { UserResponseDto } from './dto/user-response.dto';
 import { LoginUserDto } from './dto/login-user.dto';
 import { Request, Response } from 'express';
 import { AuthGuard } from '@nestjs/passport';
+import { User } from './users.entity';
+import { GetUser } from './get-user.decorator';
 
 @Controller('api/auth')
 @UsePipes(ValidationPipe)
@@ -37,8 +39,9 @@ export class AuthController {
 
     @Post('/test')
     @UseGuards(AuthGuard('jwt')) // @UseGuards 는 해당 인증 가드가 적용되는, AuthGuard는 인증가드가 어떤 전략을 사용할지 결정
-    testForAuth(@Req() req: Request) {
-        console.log(req.user); // 인증된 사용자의 정보 출력
-        return { message : 'Authenticated User', user: req.user};
+    testForAuth(@GetUser() logginedUser: User) {
+        console.log(logginedUser); // 인증된 사용자의 정보 출력
+        console.log(logginedUser.email); // 인증된 사용자의 특정 정보 출력
+        return { message : 'Authenticated User', user: logginedUser};
     }
 }
---
+++ src/auth/get-user.decorator.ts
@@ -0,0 +1,8 @@
+import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+import { User } from "./users.entity";
+
+// @GetUser 데코레이터 만들기(커스텀)
+export const GetUser = createParamDecorator((data, ctx: ExecutionContext): User => {
+    const req = ctx.switchToHttp().getRequest();
+    return req.user;
+})
